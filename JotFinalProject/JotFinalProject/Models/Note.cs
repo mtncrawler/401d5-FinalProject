bbdcb2a1ad81a168153c5ec12c8139776dd4cfa3
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-12-10 13:28:28 -0800

    added Note class

---
+++ JotFinalProject/JotFinalProject/Models/Note.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JotFinalProject.Models
+{
+    public class Note
+    {
+        public int ID { get; set; }
+
+
+    }
+}
