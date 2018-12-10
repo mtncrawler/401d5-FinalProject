79ea3cdb0d5adce950c04d51d7d8e2dca0aceca8
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-12-10 11:39:18 -0800

    added data folder with JotDbContext

---
+++ JotFinalProject/JotFinalProject/Data/JotDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JotFinalProject.Data
+{
+    public class JotDbContext : DbContext
+    {
+        public JotDbContext(DbContextOptions<JotDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
