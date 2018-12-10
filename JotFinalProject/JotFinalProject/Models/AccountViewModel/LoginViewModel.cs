1c949c131b5cef97bef151b7b5e8f87166578f68
Danul De Leon <danul.deleon@gmail.com>
2018-12-10 15:02:56 -0800

    Registration and Login working

---
+++ JotFinalProject/JotFinalProject/Models/AccountViewModel/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JotFinalProject.Models.AccountViewModel
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
