4333cf87e364b33d12ac5abb56b082c9021a3e15
Danul De Leon <danul.deleon@gmail.com>
2018-12-10 10:57:14 -0800

    home page showing

--- JotFinalProject/JotFinalProject/Startup.cs
+++ JotFinalProject/JotFinalProject/Startup.cs
@@ -15,6 +15,7 @@ namespace JotFinalProject
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +26,14 @@ namespace JotFinalProject
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            app.UseStaticFiles();
         }
     }
 }
