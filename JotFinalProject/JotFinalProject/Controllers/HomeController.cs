1a1f463699bd9a094580addeca6be67630024491
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-12-11 15:30:58 -0800

    moved partial class for api response to ImageUploaded model

--- JotFinalProject/JotFinalProject/Controllers/HomeController.cs
+++ JotFinalProject/JotFinalProject/Controllers/HomeController.cs
@@ -14,41 +14,7 @@ using Newtonsoft.Json.Linq;
 
 namespace JotFinalProject.Controllers
 {
-    public partial class Welcome
-    {
-        [JsonProperty("status")]
-        public string Status { get; set; }
-
-        [JsonProperty("recognitionResult")]
-        public RecognitionResult RecognitionResult { get; set; }
-    }
-
-    public partial class RecognitionResult
-    {
-        [JsonProperty("lines")]
-        public Line[] Lines { get; set; }
-    }
-
-    public partial class Line
-    {
-        [JsonProperty("boundingBox")]
-        public long[] BoundingBox { get; set; }
-
-        [JsonProperty("text")]
-        public string Text { get; set; }
-
-        [JsonProperty("words")]
-        public Word[] Words { get; set; }
-    }
-
-    public partial class Word
-    {
-        [JsonProperty("boundingBox")]
-        public long[] BoundingBox { get; set; }
-
-        [JsonProperty("text")]
-        public string Text { get; set; }
-    }
+    
 
     public class HomeController : Controller
     {
@@ -113,7 +79,7 @@ namespace JotFinalProject.Controllers
             string output = null;
             foreach (var item in apiReponseBody.RecognitionResult.Lines)
             {
-                output += item.Text;
+                output += item.Text + " ";
             }
 
             ViewData["data"] = output;
--- JotFinalProject/JotFinalProject/Models/ImageUploaded.cs
+++ JotFinalProject/JotFinalProject/Models/ImageUploaded.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,5 +15,41 @@ namespace JotFinalProject.Models
         public string OperationLocation { get; set; }
 
         public string ImageUrl { get; set; }
+    }
+
+    public partial class Welcome
+    {
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("recognitionResult")]
+        public RecognitionResult RecognitionResult { get; set; }
+    }
+
+    public partial class RecognitionResult
+    {
+        [JsonProperty("lines")]
+        public Line[] Lines { get; set; }
+    }
+
+    public partial class Line
+    {
+        [JsonProperty("boundingBox")]
+        public long[] BoundingBox { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("words")]
+        public Word[] Words { get; set; }
+    }
+
+    public partial class Word
+    {
+        [JsonProperty("boundingBox")]
+        public long[] BoundingBox { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
     }
 }
